c78909e8f9b9e25d909357f5d56cb62b9bc3d89b
Einar Egilsson <einar@einaregilsson.com>
2018-01-10 11:54:07 +0000

    Simplified

--- background.js
+++ background.js
@@ -1,69 +1,39 @@
-
-function clipImage(dataImage, callback) {
-	var canvas = document.createElement('canvas');
-	var canvasImg = new Image();
-
-	var ctx = canvas.getContext('2d');
-
-	canvasImg.src = dataImage;
-
-	callback(dataImage);
-}
-
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	
-	var id = sender.tab.id;
-	if (request.type === 'picture') {
-		chrome.tabs.captureVisibleTab(null, {format:'png'}, function(dataUrl) {
-			clipImage(dataUrl, function(result) {
-				sendResponse({imgData:result});
-			});
-		});
-	} else if (request.type === 'report') {
-		var dataToSend = {
-			txt: request.text,
-			img: request.image,
-			req: requests,
-			email: request.email
-		};
-		sendResponse(dataToSend);
-	} else {
-		console.log("wtf");
+	if (request.type === "picture") {
+		chrome.tabs.captureVisibleTab(null, { format: "png" }, dataUrl => sendResponse({imgData: dataUrl}));
+	} else if (request.type === "geturls") {
+		sendResponse({requests:activeTabs[sender.tab.id]});
 	}
-	
 	return true;
 });
 
 var requests = [];
-var currUrl = "";
+
+var activeTabs = {}; //Mapping of tabId => [list of requests]
+var ourDomains = /^https:\/\/cardgames\.io\/.*/;
 
 function logURL(requestDetails) {
-	chrome.tabs.query({
-		active: true,
-		lastFocusedWindow: true
-	}, function(tabs) {
-		if (tabs) {
-			currUrl = tabs[0].url;
-			var reqUrl = requestDetails.url;
-			if (!reqUrl.match('https://cardgames\.io.*') && !reqUrl.match('http://dev.cardgames.io.*') && !reqUrl.match('.*cloudfront.*') && currUrl.match('.*cardgames\.io.*')) {
-				if (tabs[0].id === requestDetails.tabId) {
-					requests.push(reqUrl);
-				}
-			}
-		} else {
-			console.log("wtf");
-		}
-	});
+    var tabId = requestDetails.tabId;
+
+    console.log('url ' + requestDetails.url + ' Matches: ' + requestDetails.url.match(ourDomains));
+    if (requestDetails.type === 'main_frame') {
+        if (requestDetails.url.match(ourDomains)) {
+            activeTabs[tabId] = []; //Reset on each page load... 
+        } else {
+            delete activeTabs[tabId];
+        }
+    } else if (typeof activeTabs[tabId] !== 'undefined') {
+        //Any other requests inside a cardgames.io page...
+        if (!requestDetails.url.match(ourDomains)) {
+            activeTabs[tabId].push(requestDetails.url);
+        } 
+    }
 }
 
-chrome.webRequest.onBeforeRequest.addListener(
-	logURL,
-	{urls: ["<all_urls>"]}
-);
+chrome.webRequest.onBeforeRequest.addListener(logURL, { urls: ["<all_urls>"] });
 
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-	if (details.frameId === 0 && details.url.match('.*cardgames\.io.*')) {
+	if (details.frameId === 0 && details.url.match(".*cardgames.io.*")) {
 		requests = [];
 	}
 });
--- content.js
+++ content.js
@@ -1,111 +1,83 @@
-var dialogHtml = document.createElement('div');
-dialogHtml.innerHTML = '<p>Please tell us your issue with this ad</p><textarea id=\"ad-text\" rows=\"5\" col=\"40\"></textarea><br><div id=\"ad-email-input\">Your email: <input type=\"text\"/></div>';
-dialogHtml.innerHTML += '<p>Screenshot of ad</p><img /><br><div id=\"ad-button-div\"><button id=\"ad-close\">Cancel</button><button id=\"ad-send\">Send</button></div></dialog>';
-dialogHtml.setAttribute('id', 'ad-dialog');
-el('#play-page').appendChild(dialogHtml);
-el('#ad-email-input input').addEventListener('input', function () {
-	var testEmail = /.+@/;
-	var currEmail = el('#ad-email-input input').value;
-	if (testEmail.test(currEmail)) {
-		el('#ad-send').disabled = false;
-	}
-	else {
-		el('#ad-send').disabled = true;
-	}
-});
-
-el('#ad-close').addEventListener('click', function() {
-	dialogHtml.style.display = "none";
-});
-el('#ad-send').addEventListener('click', function() {
-	var adText = el('#ad-text').value;
-	var email = el('#ad-email-input input').value;
-	sendReport(adText, imgSrc, email);
-	dialogHtml.style.display = "none";
-});
-
-var reportLeft = document.createElement('div');
-reportLeft.innerHTML = '<strong>Report ad</strong>';
-reportLeft.setAttribute('id', 'report-link-left');
-el('#draper-left').appendChild(reportLeft);
-
-var reportRight = document.createElement('div');
-reportRight.innerHTML = '<strong>Report ad</strong>';
-reportRight.setAttribute('id', 'report-link-right');
-el('#draper-right').appendChild(reportRight);
-
-
-reportLeft.addEventListener('click', function() {
-	//Prevent dialog opening if it already is open
-	if (dialogHtml.style.display !== "block") {
-		imgSrc = "";
-		takePicture();	
-	}
-});
-
-reportRight.addEventListener('click', function() {
-	//Prevent dialog opening if it already is open
-	if (dialogHtml.style.display !== "block") {
-		imgSrc = "";
-		takePicture();	
-	}
-});
-
-var imgSrc = "";
-
-function adDialog() {
-	dialogHtml.style.display = "block";
-
-	el('#ad-dialog img').setAttribute('src', imgSrc);
-	el('#ad-text').value = '';
-	el('#ad-email-input input').value = '';
-	el('#ad-send').disabled = true;
-
-	setTimeout(function() {
-		var dialogHeight = el('#ad-text').getBoundingClientRect().height + el('#ad-dialog img').getBoundingClientRect().height + 170;
-		dialogHtml.style.height = dialogHeight + 'px';
-	}, 10);
+function el(selector) {
+	return document.querySelector(selector);
 }
 
+var dialogHtml = document.createElement("div");
+dialogHtml.innerHTML = `
+    <h3>Please tell us your issue with this ad</h3>
+    <textarea id="ad-text" rows="5" col="40"></textarea>
+    <div id="ad-email-input">Your email: <input type="text"/></div>
+    
+    <h5>Screenshot of ad</h5>
+    <img id="screenshot" />
+    <br>
+    <div id="ad-button-div">
+        <button class="big btn" id="ad-close">Cancel</button>
+        <button class="big btn" id="ad-send">Send</button>
+    </div>
+`;
 
-function takePicture() {
-	setTimeout(function() {
-		chrome.runtime.sendMessage({
-			type: 'picture'
-		}, function(response) {
-			imgSrc = response.imgData;
-			adDialog();
-		});
-	},200);
+dialogHtml.setAttribute("id", "ad-dialog");
+
+el("#play-page").appendChild(dialogHtml);
+
+el("#ad-email-input input").addEventListener("input", function() {
+    el('#ad-send').disabled = !/.+@/.test(el("#ad-email-input input").value);
+});
+
+el("#ad-close").addEventListener("click", function() {
+	dialogHtml.style.display = "none";
+});
+
+el("#ad-send").addEventListener("click", function() {
+	sendReport(el("#ad-text").value, el('#screenshot').src, el("#ad-email-input input").value);
+	dialogHtml.style.display = "none";
+});
+
+var reportLeft = document.createElement("div");
+reportLeft.innerHTML = "<strong>Report ad</strong>";
+reportLeft.setAttribute("id", "report-link-left");
+el("#draper-left").appendChild(reportLeft);
+
+var reportRight = document.createElement("div");
+reportRight.innerHTML = "<strong>Report ad</strong>";
+reportRight.setAttribute("id", "report-link-right");
+el("#draper-right").appendChild(reportRight);
+
+function reportAd() {
+	//Prevent dialog opening if it already is open
+	if (dialogHtml.style.display !== "block") {
+		chrome.runtime.sendMessage({ type: "picture" }, response => adDialog(response.imgData)); 
+	}
+}
+reportLeft.addEventListener("click", reportAd);
+reportRight.addEventListener("click", reportAd);
+
+function adDialog(imgData) {
+	dialogHtml.style.display = "block";
+
+	el("#ad-dialog img").src = imgData;
+	el("#ad-text").value = "";
+	el("#ad-email-input input").value = "";
+	el("#ad-send").disabled = true;
 }
 
 function sendReport(txt, img, email) {
-	setTimeout(function() {
-		chrome.runtime.sendMessage({
-			type: 'report',
-			text: txt,
-			image: img,
-			email: email
-		}, function(response) {
-			
-			var dataToSend = {
-				message: response.txt,
-				image: response.img,
-				requests: response.req,
-				email: response.email
-			};
+    chrome.runtime.sendMessage({ type: "geturls" },
+        function(response) {
+            var dataToSend = {
+                message: txt,
+                image: img,
+                requests: response.requests,
+                email: email
+            };
 
-
-			var xmlhttp = new XMLHttpRequest();
-
-			var url = '/api/adreport';
-			xmlhttp.open("POST", url, true);
-			xmlhttp.setRequestHeader("Content-type", "application/json");
-			xmlhttp.send(JSON.stringify(dataToSend));
-		});
-	}, 200);
-}
-
-function el(selector) { 
-	return document.querySelector(selector); 
+            var xmlhttp = new XMLHttpRequest();
+            var url = "/api/adreport";
+            xmlhttp.open("POST", url, true);
+            xmlhttp.setRequestHeader("Content-type", "application/json");
+            xmlhttp.send(JSON.stringify(dataToSend));
+            alert('Thank you. We will look into this ad.');
+        }
+    );
 }
